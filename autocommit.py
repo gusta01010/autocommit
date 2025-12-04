ec750267fb4798c6b1c260b77dde25588a32dbfd
Gustavo Lima <sonic8783@hotmail.com>
2025-12-04 11:16:11 -0300

    fix: file_path para file em metodo

--- autocommit.py
+++ autocommit.py
@@ -253,7 +253,7 @@ def main():
                 return
 
             # Verifica o repositório Git
-            if not verificar_repositorio(file_path):
+            if not verificar_repositorio(file):
                 return
 
             # Obtém alterações
