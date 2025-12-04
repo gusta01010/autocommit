70da56c6b110dcd290eaff9332a9ea3224a40fb0
Gustavo Lima <sonic8783@hotmail.com>
2025-12-04 02:13:49 -0300

    feat: configuração de prompt por JSON

--- autocommit.py
+++ autocommit.py
@@ -4,7 +4,7 @@ import argparse
 import subprocess
 import requests
 from dotenv import load_dotenv
-
+import json
 
 
 # Carrega as variáveis de ambiente do arquivo .env
@@ -121,6 +121,18 @@ def obter_alteracoes():
         print(f"❌ Erro ao obter alterações: {e}")
         return None
 
+# configura o prompt a partir do JSON
+def setPrompt(diff_text):
+    with open('./prompt.json', 'r', encoding='utf-8') as data:
+        prompt = json.load(data)
+        
+        #filtro para substituir palavras
+        prompt["prompt"] = prompt["prompt"].replace('{getIdioma}',getIdioma())
+        prompt["prompt"] = prompt["prompt"].replace('{diff_text}',diff_text)
+    
+    return prompt["prompt"]
+        
+
 def gerar_mensagem_commit(diff_text):
     """Gera uma mensagem de commit usando a API do Gemini"""
     # Lista de modelos para tentar em ordem
@@ -129,13 +141,8 @@ def gerar_mensagem_commit(diff_text):
         'gemini-2.5-flash-lite',  # Versão mais rápida em resposta
         'gemini-2.5-pro'  # Versão pro
     ]
-    
-    prompt = (
-        f"Faça em {getIdioma()}, gere uma mensagem de commit detalhada " #retorna idioma recebido no parâmetro
-        "com base nas seguintes diferenças entre os arquivos. "
-        "Sua primeira linha na resposta deve ser o título:\n"
-        f"{diff_text}"
-    )
+
+    prompt = setPrompt(diff_text)
     
     print("🔄 Tentando gerar mensagem com API do Gemini...")
     
